05a51dc809e077ac0bc9dd94e0c8f56784a28087
Vanica <88245937+VanicaK@users.noreply.github.com>
2024-03-16 18:35:34 +0000

    Add files via upload

---
+++ exec.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window=Tk()
+window.title="FILE DOWNLOAD SUCCESSFUL"
+
+successLabel=Label(text="SUCCESS!")
+successLabel.place(relx=0.5,rely=0.5,anchor=CENTER)
+
+window.mainloop()
